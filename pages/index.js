31ad3d9952838c2e8f4b5645bc06f4b0b0b01327
Spid3rrr <bra.ami20@hotmail.fr>
2023-02-12 12:44:33 +0100

    Added transition animation

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ import Cta from "../components/cta";
 import Faq from "../components/faq";
 import PopupWidget from "../components/popupWidget";
 
+import { motion } from "framer-motion";
 //import dynamic from "next/dynamic";
 
 // const Video = dynamic(() => import("../components/video"));
@@ -38,33 +39,77 @@ export default function Home() {
 
       <Navbar />
       <Hero />
-      <SectionTitle
-        pretitle="Our services"
-        title=" Why choose us ?">
-        Experience a new level of efficiency, accuracy, and accessibility.
-         Whether you're a business looking to streamline your operations or an individual 
-         seeking a smarter, more connected lifestyle, we got you covered.
-      </SectionTitle>
-      <Benefits data={benefitOne} />
-      <Benefits imgPos="right" data={benefitTwo} />
-      <SectionTitle
-        pretitle="Demonstration"
-        title="Learn how we can help you grow">
-        You can check out this video from one of our clients highlighting the growth he has made and how our products eased his way to success
-      </SectionTitle>
-      <Video />
-      <SectionTitle
-        pretitle="Testimonials"
-        title="Here's what our customers said">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <SectionTitle pretitle="Our services" title=" Why choose us ?">
+          Experience a new level of efficiency, accuracy, and accessibility.
+          Whether you're a business looking to streamline your operations or an
+          individual seeking a smarter, more connected lifestyle, we got you
+          covered.
+        </SectionTitle>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <Benefits data={benefitOne} />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <Benefits imgPos="right" data={benefitTwo} />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <SectionTitle
+          pretitle="Demonstration"
+          title="Learn how we can help you grow"
+        >
+          You can check out this video from one of our clients highlighting the
+          growth he has made and how our products eased his way to success
+        </SectionTitle>
+        <Video />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <SectionTitle
+          pretitle="Testimonials"
+          title="Here's what our customers said"
+        >
           Here is some feedback from our customers !
-      </SectionTitle>
-      <Testimonials />
-      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
-        These are some frequently asked questions from our customers
-      </SectionTitle>
-      <Faq />
-      <Cta />
-      <Footer />
+        </SectionTitle>
+        <Testimonials />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
+          These are some frequently asked questions from our customers
+        </SectionTitle>
+        <Faq />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <Cta />
+        <Footer />
+      </motion.div>
       <PopupWidget />
     </>
   );
